41a5ba92028d126a8e084a40f608debd16ce5ff6
Gael250 <160473558+Gael250@users.noreply.github.com>
2025-05-13 10:02:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express= require('express')
 const app= express()
 const port= 3000
 app.get('/',(req,res)=>{
-    res.sendFile('/public/index.html',{root: __dirname})
+    res.sendFile('/public/dashboard.html',{root: __dirname})
+})
+app.get('/tictactoe',(req,res)=>{
+    res.sendFile('/public/tictactoe.html',{root:__dirname})
+})
+app.get('/simonGame',(req,res)=>{
+    res.sendFile('/public/simonGame.html',{root:__dirname})
 })
 app.listen(port, () => {    
     console.log(`Server is running on http://localhost:${port}`)
