3b246a43145253b6a3b8897e5206bd80c8504370
Gael250 <160473558+Gael250@users.noreply.github.com>
2025-05-15 10:08:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 // Serve static files (like CSS, JS, images)
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const scoreRoutes = require('./routes/score.route.js'); // adjust the path if needed
+app.use('/api', scoreRoutes);
 
 // Dashboard Route
 app.get('/', (req, res) => {
