55ebc5d3d75496cc739410ed007b0c9edfbd1c85
Gael250 <160473558+Gael250@users.noreply.github.com>
2025-05-15 10:12:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
+const cors = require('cors');
+app.use(cors());
 
 // Serve static files (like CSS, JS, images)
 app.use(express.static(path.join(__dirname, 'public')));
